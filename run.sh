cb2b18eb20e22b4e8d72777fdd9ef8c6ba3702c3
Keith Knott <keith.knott@gmail.com>
2024-05-20 23:45:45 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-brew install postgresql
+brew install postgresql pipx
 export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
-pip install -r requirements.txt
+pipx install -r requirements.txt
 python3 -c "import nltk; nltk.download('stopwords')"
 python3 main.py --log-dir=input_logs
