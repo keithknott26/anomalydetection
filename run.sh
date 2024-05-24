9ba038316057275799448d596d6fb2dc20c2c4f3
Keith Knott <keith.knott@gmail.com>
2024-05-23 20:49:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ cd termcolor
 python3 -m pip install .
 cd ..
 export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
+brew install pipx
 pipx install -r requirements.txt
 python3 -c "import nltk; nltk.download('stopwords')"
 python3 main.py --log-dir=input_logs
