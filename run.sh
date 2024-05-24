ca1c80098f048c1607ab56f87e6e5ea9bafbd57b
Keith Knott <keith.knott@gmail.com>
2024-05-23 20:59:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ python3 -m pip install .
 cd ..
 export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 brew install pipx
+pip3 install nltk numpy scikit-learn joblib prettytable python-Levenshtein APScheduler psycopg2-binary drain3 prettytable Levenshtein termcolor rich dash loguru joblib python-dateutil
 pipx install -r requirements.txt
 python3 -c "import nltk; nltk.download('stopwords')"
 python3 main.py --log-dir=input_logs
